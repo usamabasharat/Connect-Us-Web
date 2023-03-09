e469e52d81dbbf41791986b60d981893510a5b84
Ans1265 <ans.abdulwaheed@devsinc.com>
2023-03-09 15:11:08 +0500

    Configuratuin of eslint & eslint-config-airbnb.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@javascript-eslint/recommended",
+        "airbnb"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
