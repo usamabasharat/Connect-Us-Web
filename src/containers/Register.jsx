a1226215044ed610eda537ee590a5590dd96d142
Ans1265 <ans.abdulwaheed@devsinc.com>
2023-03-15 15:56:29 +0500

    Implemented Shared Component (TexField) and set up Tailwind.

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,14 +1,8 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
-import {
-  LockOutlined, UserOutlined, FrownOutlined, SmileOutlined,
-} from '@ant-design/icons';
-import {
-  Button, Form, Input, notification,
-} from 'antd';
+import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Button, Form, notification } from 'antd';
 import { updateUsers, credentials } from '../constants/credentials';
+import Textfield from '../shared/TextField';
 
 /*
   @Register Function
@@ -31,24 +25,21 @@ function Register() {
         updateUsers(newUser);
         notification.open({
           message: 'Success',
-          description:
-            'User SignedUp Successfuly.',
-          icon: <SmileOutlined style={{ color: '#108ee9' }} />,
+          description: 'User SignedUp Successfuly.',
+          icon: <SmileOutlined style={{ color: '#108ee9' }} />
         });
       } else {
         notification.open({
           message: 'Error',
-          description:
-          'Email Already exist.',
-          icon: <FrownOutlined style={{ color: '#108ee9' }} />,
+          description: 'Email Already exist.',
+          icon: <FrownOutlined style={{ color: '#108ee9' }} />
         });
       }
     } else {
       notification.open({
         message: 'Error',
-        description:
-          'Both password do not match.',
-        icon: <FrownOutlined style={{ color: '#108ee9' }} />,
+        description: 'Both password do not match.',
+        icon: <FrownOutlined style={{ color: '#108ee9' }} />
       });
     }
   };
@@ -63,94 +54,83 @@ function Register() {
           initialValues={{ remember: true }}
           onFinish={onFinish}
         >
-          <Form.Item
+          <Textfield
             name="First Name"
-            rules={[{
-              type: 'text', required: true, message: 'Please input your First Name!',
-            }]}
-          >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              First Name
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="FIrst Name" />
-            </label>
-          </Form.Item>
-          <Form.Item
+            rules={[
+              {
+                type: 'text',
+                required: true,
+                message: 'Please input your First Name!'
+              }
+            ]}
+            type="text"
+          />
+          <Textfield
             name="Last Name"
-            rules={[{
-              type: 'text', required: true, message: 'Please input your Last Name!',
-            }]}
-          >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Last Name
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Last Name" />
-            </label>
-          </Form.Item>
-          <Form.Item
-            name="number"
-            rules={[{
-              type: 'text', required: true, message: 'Please input a valid number!', pattern: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
-            }]}
-          >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Number
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Number" />
-            </label>
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[{
-              type: 'email', required: true, message: 'Please input your valid email!',
-            }]}
-          >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="email" />
-            </label>
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' },
+            rules={[
+              {
+                required: true,
+                message: 'Please input your First Name!'
+              }
+            ]}
+            type="text"
+          />
+          <Textfield
+            name="Number"
+            rules={[
+              {
+                required: true,
+                message: 'Please input a valid number!',
+                pattern: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/
+              }
+            ]}
+            type="text"
+          />
+          <Textfield
+            name="Email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your valid email!'
+              }
+            ]}
+            type="email"
+          />
+          <Textfield
+            name="Password"
+            rules={[
+              { required: true, message: 'Please input your Password!' },
               { min: 8, message: 'Password must be minimum 8 characters.' },
               {
                 pattern: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9!#@$%\-_=+<>]+)$/,
-                message: 'Password Pattern',
-              },
+                message: 'Password not strong enough!'
+              }
             ]}
-          >
-            <label className="block text-left text-gray-700 text-sm font-bold mb-4">
-              Password
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
-            </label>
-          </Form.Item>
-          <Form.Item
-            name="password1"
-            rules={[{ required: true, message: 'Please input your Password!' },
+            type="password"
+          />
+          <Textfield
+            name="Re-Enter Password"
+            rules={[
+              { required: true, message: 'Please input your Password!' },
               { min: 8, message: 'Password must be minimum 8 characters.' },
               {
                 pattern: /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9!#@$%\-_=+<>]+)$/,
-                message: 'Password Pattern',
-              },
+                message: 'Password not strong enough!'
+              }
             ]}
-          >
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              Confirm Password
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Re-Password"
-              />
-            </label>
-          </Form.Item>
+            type="password"
+          />
           <Form.Item>
             <div className="flex justify-between">
-              <Button htmlType="submit" className="login-form-button text-white border-[#008080] bg-[#008080] hover:bg-[#20b2aa] hover:text-white w-1/3">
+              <Button
+                htmlType="submit"
+                className="login-form-button text-white border-[#008080] bg-[#008080] hover:bg-[#20b2aa] hover:text-white w-1/3"
+              >
                 Sign up
               </Button>
-              <a href="#" className="text-[#008080] hover:text-[#20b2aa]">Already a user Sign in</a>
+              <div href="#" className="text-[#008080] hover:text-[#20b2aa]">
+                Already a user Sign in
+              </div>
             </div>
           </Form.Item>
         </Form>
