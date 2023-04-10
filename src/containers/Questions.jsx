b85e07be76b5de0478ed2b71fa23298e0b9a4f6e
Ans1265 <ans.abdulwaheed@devsinc.com>
2023-04-10 19:20:25 +0500

    Redesigned Questions.jsx

--- src/containers/Questions.jsx
+++ src/containers/Questions.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-  Form, Radio, Checkbox, Button, Select, Input, notification
+  Form, Radio, Checkbox, Button, Select, Input, notification, Card
 } from 'antd';
 import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
@@ -17,6 +17,7 @@ function Questions() {
   const { user } = useSelector((state) => state.user);
 
   const handleSubmit = async (values) => {
+    console.log(values);
     const createdBy = user.id;
     if (typeof values.Options === 'string') {
       answersArray.push(values.Options);
@@ -83,51 +84,56 @@ function Questions() {
 
   return (
     <div className="py-60 max-w-3xl mx-auto">
+      <h1 className="text-[#008080] text-3xl pb-8">Create Questions</h1>
       <Form form={form} onFinish={handleSubmit}>
-        <Form.Item label="Question Type" rules={[{ required: true }]}>
-          <Select
-            className="mb-5 w-max"
-            defaultValue=""
-            onChange={handleQuestionChange}
-          >
-            <Select.Option value="mock">Mock</Select.Option>
-            <Select.Option value="codereview">Code Review</Select.Option>
-            <Select.Option value="one">One on One</Select.Option>
-            <Select.Option value="annual">Annual Review</Select.Option>
-            <Select.Option value="biannual">Biannual Review</Select.Option>
-            <Select.Option value="quarterly">Quarterly Review</Select.Option>
-          </Select>
-        </Form.Item>
-        <Textfield
-          name="text"
-          labelText="Question"
-          placeholder="Question"
-          rules={[{ required: true, message: QUESTION_REQUIRED }]}
-          type="text"
-        />
-        <Form.Item label="Answer Type" rules={[{ required: true }]}>
-          <Select
-            className="mb-5 w-max"
-            defaultValue=""
-            onChange={handleChange}
-          >
-            <Select.Option value="options">Checkbox</Select.Option>
-            <Select.Option value="boolean">Radio Button</Select.Option>
-            <Select.Option value="numeric">Select</Select.Option>
-            <Select.Option value="string">Text</Select.Option>
-          </Select>
-        </Form.Item>
-        {answerType === 'boolean' && (
-        <Form.Item className="text-left" name="Options" label="Options">
-          <Radio.Group>{Options}</Radio.Group>
-        </Form.Item>
-        )}
-        {answerType === 'options' && (
+        <Card title="Question Type" className="max-w-xl mx-auto">
+          <Form.Item rules={[{ required: true }]}>
+            <Select
+              className="mb-5 w-max"
+              defaultValue=""
+              onChange={handleQuestionChange}
+            >
+              <Select.Option value="mock">Mock</Select.Option>
+              <Select.Option value="codereview">Code Review</Select.Option>
+              <Select.Option value="one">One on One</Select.Option>
+              <Select.Option value="annual">Annual Review</Select.Option>
+              <Select.Option value="biannual">Biannual Review</Select.Option>
+              <Select.Option value="quarterly">Quarterly Review</Select.Option>
+            </Select>
+          </Form.Item>
+        </Card>
+        <Card title="Question" className="max-w-xl mx-auto">
+          <Textfield
+            name="text"
+            placeholder="Question"
+            rules={[{ required: true, message: QUESTION_REQUIRED }]}
+            type="text"
+          />
+        </Card>
+        <Card title="Answer Type" className="max-w-xl mx-auto">
+          <Form.Item rules={[{ required: true }]}>
+            <Select
+              className="mb-5 w-max"
+              defaultValue=""
+              onChange={handleChange}
+            >
+              <Select.Option value="options">Checkbox</Select.Option>
+              <Select.Option value="boolean">Radio Button</Select.Option>
+              <Select.Option value="numeric">Select</Select.Option>
+              <Select.Option value="string">Text</Select.Option>
+            </Select>
+          </Form.Item>
+          {answerType === 'boolean' && (
+          <Form.Item className="text-left" name="Options" label="Options">
+            <Radio.Group>{Options}</Radio.Group>
+          </Form.Item>
+          )}
+          {answerType === 'options' && (
           <Form.Item className="text-left" name="Options" label="Options">
             <Checkbox.Group className="flex-col">{Options}</Checkbox.Group>
           </Form.Item>
-        )}
-        {answerType === 'numeric' && (
+          )}
+          {answerType === 'numeric' && (
           <Form.Item className="text-left" name="Options" label="Options">
             <Select
               className="mb-5 w-max"
@@ -135,28 +141,21 @@ function Questions() {
               {Options}
             </Select>
           </Form.Item>
-        )}
+          )}
+        </Card>
         {answerType !== 'string' && (
-        <div className="mb-16 flex">
-          <div className="w-full">
-            <label htmlFor="add option" className="block text-gray-700 text-sm font-bold mb-2 text-left">
-              Add a Option
-              <Input className="w-full" value={newOption} onChange={changeHandler} placeholder="Add Option" />
-            </label>
-          </div>
-          <Button
-            onClick={addOption}
-            className="mt-5 text-white border-[#008080] bg-[#008080] hover:bg-[#20B2AA] hover:!text-white hover:!border-[#20B2AA]"
-          >
-            Add Answer
-          </Button>
-        </div>
+          <Card title="Add a Option" className="max-w-xl mx-auto">
+            <Input className="w-full" value={newOption} onChange={changeHandler} placeholder="Add Option" />
+            <Button
+              onClick={addOption}
+              className="mt-5 text-white border-[#008080] bg-[#008080] hover:bg-[#20B2AA] hover:!text-white hover:!border-[#20B2AA]"
+            >
+              Add Answer
+            </Button>
+          </Card>
         )}
         <Form.Item>
-          <Button
-            htmlType="submit"
-            className="login-form-button text-white border-[#008080] bg-[#008080] hover:bg-[#20B2AA] hover:!text-white hover:!border-[#20B2AA]"
-          >
+          <Button htmlType="submit">
             Add Question
           </Button>
         </Form.Item>
